16718fc287193cdebd5af053e93445490e51e927
Shahar Yair <shaharyair12@gmail.com>
2023-07-30 20:59:14 +0300

    fix:homepage header

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ function Page() {
     <>
       <div className='h-screen -mt-28 flex justify-center items-center'>
         <div className='flex justify-center items-center gap-8 text-center m-5'>
-          <p className='text-white text-3xl lg:text-4xl'>
-            We bring the <span className='text-pinklogo'>Spot</span>, To your
+          <p className='text-white text-xl md:text-4xl'>
+            We bring the <span className='text-pinklogo'>Spot.</span> To your
             music.
           </p>
         </div>
