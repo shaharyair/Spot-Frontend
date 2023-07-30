3d585f36074aa91be05a1fb2dc986b9143bc906e
Shahar Yair <shaharyair12@gmail.com>
2023-07-30 15:17:15 +0300

    fix:navbar functionality

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -17,10 +17,6 @@ function Navbar() {
     setOpenMobileNavbar(!openMobileNavbar);
   };
 
-  const closeMobileNavbar = () => {
-    setOpenMobileNavbar(false);
-  };
-
   useEffect(() => {
     const closeOnResize = () => {
       if (window.innerWidth >= 1024) {
@@ -28,12 +24,34 @@ function Navbar() {
       }
     };
 
+    const handleLinkClick = (event) => {
+      // Check if the click target is a link or a descendant of a link
+      const isLinkClicked = event.target.closest("a");
+
+      // If a link was clicked, close the mobile navbar
+      if (isLinkClicked) {
+        setOpenMobileNavbar(false);
+      }
+    };
+
+    const handleOutsideClick = (event) => {
+      // Check if the click target is outside the navbar container
+      const navbarContainer = document.querySelector(".container");
+      if (navbarContainer && !navbarContainer.contains(event.target)) {
+        setOpenMobileNavbar(false);
+      }
+    };
+
+    document.addEventListener("click", handleLinkClick);
+    document.addEventListener("click", handleOutsideClick);
     window.addEventListener("resize", closeOnResize);
 
     return () => {
+      document.removeEventListener("click", handleLinkClick);
+      document.removeEventListener("click", handleOutsideClick);
       window.removeEventListener("resize", closeOnResize);
     };
-  });
+  }, [openMobileNavbar]);
 
   return (
     <>
@@ -62,7 +80,6 @@ function Navbar() {
                   <Link
                     href='/tracks'
                     className='block px-2 py-1 hover:text-white transition-colors duration-200'
-                    onClick={closeMobileNavbar}
                   >
                     Display Tracks
                   </Link>
@@ -71,7 +88,6 @@ function Navbar() {
                   <Link
                     href='/add-track'
                     className='block px-2 py-1 hover:text-white transition-colors duration-200'
-                    onClick={closeMobileNavbar}
                   >
                     Add Track
                   </Link>
@@ -80,52 +96,43 @@ function Navbar() {
                   <Link
                     href='/delete-track'
                     className='block px-2 py-1 hover:text-white transition-colors duration-200'
-                    onClick={closeMobileNavbar}
                   >
                     Delete Track
                   </Link>
                 </li>
               </ul>
             </nav>
-            <Link
-              href='/account'
-              className='inline-block lg:hidden'
-              onClick={closeMobileNavbar}
-            >
+            <Link href='/account' className='inline-block lg:hidden'>
               <div className='mb-10 flex items-center gap-3 text-pinklogo hover:text-white transition-colors duration-200'>
                 <HiUser className='text-2xl' />
                 <p className='text-lg'>Sign In</p>
               </div>
             </Link>
             <div className='flex gap-4 lg:hidden text-3xl text-pinklogo'>
-              <Link href='/' onClick={closeMobileNavbar}>
+              <Link href='/'>
                 <FaInstagram className=' hover:text-white transition-colors duration-200' />
               </Link>
-              <Link href='/' onClick={closeMobileNavbar}>
+              <Link href='/'>
                 <FaFacebookSquare className=' hover:text-white transition-colors duration-200' />
               </Link>
-              <Link href='/' onClick={closeMobileNavbar}>
+              <Link href='/'>
                 <FaYoutube className=' hover:text-white transition-colors duration-200' />
               </Link>
-              <Link href='/' onClick={closeMobileNavbar}>
+              <Link href='/'>
                 <FaTiktok className=' hover:text-white transition-colors duration-200' />
               </Link>
             </div>
           </div>
-          <Link
-            href='/'
-            className='ml-6 lg:ml-0 lg:mr-10'
-            onClick={closeMobileNavbar}
-          >
+          <Link href='/' className='ml-6 lg:ml-0 lg:mr-10'>
             <h1 className='text-4xl lg:text-5xl font-semibold text-pinklogo'>
               Spot.
             </h1>
           </Link>
           <div className='flex items-center justify-center gap-5 lg:order-3 text-2xl text-pinklogo lg:text-3xl'>
-            <Link href='/' onClick={closeMobileNavbar}>
+            <Link href='/'>
               <HiMagnifyingGlass className='hover:text-white transition-colors duration-200' />
             </Link>
-            <Link href='/account' onClick={closeMobileNavbar}>
+            <Link href='/account'>
               <HiUser className='hover:text-white transition-colors duration-200' />
             </Link>
           </div>
