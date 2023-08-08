c091c64766f20933ccc96c2f1fbc58d3e80b03b9
Shahar Yair <shaharyair12@gmail.com>
2023-08-08 20:20:32 +0300

    feat: added loading bar, error/success messages for add-track

--- app/add-track/page.js
+++ app/add-track/page.js
@@ -4,10 +4,15 @@ import axios from "axios";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import LoadingBar from "@/components/loadingbar";
+import ErrorMessage from "@/components/errormessage";
 
 function Page() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [songTitle, setSongTitle] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [songUploaded, setSongUploaded] = useState(false);
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -25,6 +30,10 @@ function Page() {
       return;
     }
 
+    setError(null);
+    setSongUploaded(false);
+    setLoading(true);
+
     const formData = new FormData();
     formData.append("file", selectedFile);
     formData.append("title", songTitle);
@@ -32,44 +41,55 @@ function Page() {
     axios
       .post("http://127.0.0.1:5000/api/upload_song", formData)
       .then((response) => {
-        console.log("Song uploaded successfully!");
-        alert("Song uploaded successfully!");
+        setSongUploaded(true);
       })
       .catch((error) => {
-        console.error("Failed to upload the song.", error);
-        alert("Failed to upload the song.");
+        setError("Error adding song. Please try again later.");
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
 
   return (
     <>
       <div className='container h-[98vh] min-h-[650px] flex justify-center items-center'>
-        <form
-          className='flex flex-col justify-center items-center gap-8 text-center'
-          onSubmit={handleUpload}
-        >
-          <Input
-            className='w-[90vw] max-w-[300px] min-w-[250px]'
-            id='Track'
-            type='file'
-            onChange={handleFileChange}
-            required
-          />
-          <Input
-            className='w-[90vw] max-w-[300px] min-w-[250px] text-md lg:text-lg'
-            type='text'
-            value={songTitle}
-            onChange={handleTitleChange}
-            placeholder='Track Title'
-          />
-          <Button
-            type='submit'
-            disabled={!selectedFile}
-            className='bg-bpmPink text-black hover:bg-white duration-200 '
+        {!error && loading ? (
+          <LoadingBar />
+        ) : (
+          <form
+            className='flex flex-col justify-center items-center gap-8 text-center'
+            onSubmit={handleUpload}
           >
-            Upload Track
-          </Button>
-        </form>
+            {songUploaded && (
+              <h2 className='text-white text-lg lg:text-2xl'>
+                Song uploaded successfully!
+              </h2>
+            )}
+            {error && <ErrorMessage message={error} />}
+            <Input
+              className='w-[90vw] max-w-[300px] min-w-[250px]'
+              id='Track'
+              type='file'
+              onChange={handleFileChange}
+              required
+            />
+            <Input
+              className='w-[90vw] max-w-[300px] min-w-[250px] text-md lg:text-lg'
+              type='text'
+              value={songTitle}
+              onChange={handleTitleChange}
+              placeholder='Title'
+            />
+            <Button
+              type='submit'
+              disabled={!selectedFile}
+              className='bg-bpmPink text-black hover:bg-white duration-200 '
+            >
+              Upload Track
+            </Button>
+          </form>
+        )}
       </div>
     </>
   );
