b418acf6b9aac154144b9c867a0d3318be904503
Shahar Yair <shaharyair12@gmail.com>
2023-08-09 01:48:50 +0300

    fix: fixed main heading text-size

--- app/page.js
+++ app/page.js
@@ -2,11 +2,11 @@ function Page() {
   return (
     <>
       <div className='container h-[98vh] min-h-[650px] flex justify-center items-center'>
-        <div className='flex justify-center items-center gap-8 text-center'>
-          <p className='text-white text-xl md:text-4xl'>
+        <div className='flex justify-center items-center gap-8 text-center font-thin text-white text-xl md:text-2xl lg:text-4xl leading-8'>
+          <h1>
             We bring the <span className='text-bpmPink'>Spot.</span> To your
             music.
-          </p>
+          </h1>
         </div>
       </div>
     </>
