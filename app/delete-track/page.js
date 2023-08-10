2fa5086f1c60de22b1fcc28db8af7ba5bc8a5656
Shahar Yair <shaharyair12@gmail.com>
2023-08-10 18:47:30 +0300

    WIP

--- app/delete-track/page.js
+++ app/delete-track/page.js
@@ -2,27 +2,52 @@
 
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
 import LoadingBar from "@/components/loadingbar";
+import { HiTrash } from "react-icons/hi2";
 import ErrorMessage from "@/components/errormessage";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const FormSchema = z.object({
+  id: z
+    .string()
+    .nonempty({ message: "Please select a track to delete." })
+    .regex(/^[0-9]+$/, { message: "Invalid track ID format." })
+    .min(1, { message: "Invalid track ID format." }),
+});
 
 function SongListDeleteForm({ songs }) {
-  const [songTitle, setSongTitle] = useState("");
+  const form = useForm({
+    resolver: zodResolver(FormSchema),
+  });
 
-  const handleRadioChange = (event) => {
-    setSongTitle(event.target.value);
-  };
+  function onSubmit(data) {
+    const songIdInt = parseInt(data.id);
 
-  const handleDelete = (event) => {
-    event.preventDefault();
-    if (!songTitle.trim()) {
-      alert("Please choose a song title.");
-      return;
-    }
+    console.log("Form submitted with data:", songIdInt);
 
     axios
-      .post("http://127.0.0.1:5000/api/delete_song", { title: songTitle })
+      .post("http://127.0.0.1:5000/api/delete_song", { id: songIdInt })
       .then((response) => {
         console.log("Song deleted successfully!");
         alert("Song deleted successfully!");
@@ -32,43 +57,59 @@ function SongListDeleteForm({ songs }) {
         console.error("Failed to delete the song.", error);
         alert("Failed to delete the song!");
       });
-  };
+  }
 
   return (
     <>
-      <form
-        className='flex flex-col justify-center items-center gap-8 text-center'
-        onSubmit={handleDelete}
-      >
-        <h2 className='text-bpmPink text-3xl lg:text-4xl'>Tracks:</h2>
-        <ul className='flex flex-col justify-center items-start gap-2 text-left'>
-          {songs.map((song, index) => (
-            <li key={index} className='flex justify-center items-center gap-2'>
-              <input
-                id={song.title}
-                type='radio'
-                value={song.title}
-                checked={songTitle === song.title}
-                onChange={handleRadioChange}
-                className='h-4 w-4'
-              />
-              <Label
-                htmlFor={song.title}
-                className='text-white md:text-md lg:text-lg leading-5'
-              >
-                {song.title}
-              </Label>
-            </li>
-          ))}
-        </ul>
-        <Button
-          disabled={!songTitle}
-          type='submit'
-          className='bg-bpmPink text-black hover:bg-white duration-200 '
-        >
-          Delete Track
-        </Button>
-      </form>
+      <div className='flex flex-col justify-center items-center gap-8'>
+        <h2 className='text-3xl lg:text-4xl text-white font-thin'>
+          <span className='text-bpmPink'>Delete</span> your tracks.
+        </h2>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='flex flex-col justify-center items-center gap-8'
+          >
+            <FormField
+              control={form.control}
+              name='id'
+              render={({ field }) => (
+                <FormItem className='w-[90vw] max-w-[300px] min-w-[250px] text-md lg:text-lg'>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder='Select a track' />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Tracks</SelectLabel>
+                        {songs.map((song, index) => (
+                          <SelectItem key={index} value={song.id}>
+                            {song.title}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              size='lg'
+              type='submit'
+              className='bg-bpmPink text-black hover:bg-white duration-200 flex justify-center items-center gap-2'
+            >
+              Delete
+              <HiTrash className='text-lg lg:text-xl' />
+            </Button>
+          </form>
+        </Form>
+      </div>
     </>
   );
 }
