165995892008d68cb840cae7a45eb708ff0cc838
Shahar Yair <shaharyair12@gmail.com>
2023-08-11 15:09:41 +0300

    WIP 2

--- app/delete-track/page.js
+++ app/delete-track/page.js
@@ -9,15 +9,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import {
   Select,
   SelectContent,
@@ -28,100 +20,122 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import WarningDialog from "@/components/warning-dialog";
+import Dialog from "@/components/dialog";
 
 const FormSchema = z.object({
-  id: z
-    .string()
-    .nonempty({ message: "Please select a track to delete." })
-    .regex(/^[0-9]+$/, { message: "Invalid track ID format." })
-    .min(1, { message: "Invalid track ID format." }),
+  id: z.number({}),
 });
 
-function SongListDeleteForm({ songs }) {
-  const [openDialog, setOpenDialog] = useState(true);
+function SongListDeleteForm({ songs, onSongDeleted }) {
+  const [openWarningDialog, setOpenWarningDialog] = useState(false);
+  const [songDeleted, setSongDeleted] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [selectedSongId, setSelectedSongId] = useState(null);
 
   const form = useForm({
     resolver: zodResolver(FormSchema),
   });
 
   function onSubmit(data) {
-    const songIdInt = parseInt(data.id);
-
-    console.log("Form submitted with data:", songIdInt);
+    setLoading(true);
 
     axios
-      .post("http://127.0.0.1:5000/api/delete_song", { id: songIdInt })
+      .post("http://127.0.0.1:5000/api/delete_song", data)
       .then((response) => {
-        console.log("Song deleted successfully!");
-        alert("Song deleted successfully!");
-        window.location.reload();
+        setOpenWarningDialog(false);
+        setSongDeleted("Track deleted successfully.");
+        setLoading(false);
       })
       .catch((error) => {
-        console.error("Failed to delete the song.", error);
-        alert("Failed to delete the song!");
+        setOpenWarningDialog(false);
+        setError(error.message);
+        setLoading(false);
       });
   }
 
   return (
     <>
-      <div className='flex flex-col justify-center items-center gap-8'>
-        <h2 className='text-3xl lg:text-4xl text-white font-thin'>
-          <span className='text-bpmPink'>Delete</span> your tracks.
-        </h2>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className='flex flex-col justify-center items-center gap-8'
-          >
-            <FormField
-              control={form.control}
-              name='id'
-              render={({ field }) => (
-                <FormItem className='w-[90vw] max-w-[300px] min-w-[250px] text-md lg:text-lg'>
-                  <Select
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder='Select a track' />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectLabel>Tracks</SelectLabel>
-                        {songs.map((song, index) => (
-                          <SelectItem key={index} value={song.id}>
-                            {song.title}
-                          </SelectItem>
-                        ))}
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button
-              onClick={() => setOpenDialog(true)}
-              size='lg'
-              className='bg-bpmPink text-black hover:bg-white duration-200 flex justify-center items-center gap-2'
+      {!loading && songDeleted && (
+        <Dialog message={songDeleted} onClick={() => setSongDeleted(false)} />
+      )}
+      {!loading && error && (
+        <Dialog message={error} onClick={() => setError(false)} />
+      )}
+      {loading ? (
+        <LoadingBar />
+      ) : (
+        <div className='flex flex-col justify-center items-center gap-10 w-[90vw] max-w-[300px] min-w-[250px] text-md lg:text-lg'>
+          <h2 className='text-3xl lg:text-4xl text-white font-thin overflow-visible whitespace-nowrap'>
+            <span className='text-bpmPink'>Delete</span> your tracks.
+          </h2>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className='flex flex-col justify-center items-center gap-8'
             >
-              Delete
-              <HiTrash className='text-lg lg:text-xl' />
-            </Button>
-            {openDialog && (
-              <WarningDialog
-                title={"Are you absolutely sure?"}
-                message={
-                  "This action cannot be undone. This will permanently delete the track."
-                }
-                onClick={() => setOpenDialog(false)}
+              <FormField
+                control={form.control}
+                name='id'
+                render={({ field }) => (
+                  <FormItem className='w-[90vw] max-w-[300px] min-w-[250px] text-md lg:text-lg'>
+                    <Select
+                      onValueChange={(value) => {
+                        const selectedSong = songs.find(
+                          (song) => song.title === value
+                        );
+
+                        console.log(selectedSong);
+
+                        setSelectedSongId(selectedSong.id);
+
+                        console.log(selectedSongId);
+
+                        field.onChange(selectedSongId);
+                      }}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder='Select a track' />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Tracks</SelectLabel>
+                          {songs.map((song, index) => (
+                            <SelectItem key={index} value={song.title}>
+                              {song.title}
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
               />
-            )}
-          </form>
-        </Form>
-      </div>
+              <Button
+                // onClick={() => setOpenWarningDialog(true)}
+                size='lg'
+                className='bg-bpmPink text-black hover:bg-white duration-200 flex justify-center items-center gap-2'
+                type='submit'
+              >
+                Delete
+                <HiTrash className='text-lg lg:text-xl' />
+              </Button>
+              {openWarningDialog && (
+                <WarningDialog
+                  title={"Are you sure?"}
+                  message={
+                    "This action cannot be undone. This will permanently delete the track."
+                  }
+                  onClickCancel={() => setOpenWarningDialog(false)}
+                />
+              )}
+            </form>
+          </Form>
+        </div>
+      )}
     </>
   );
 }
@@ -130,6 +144,12 @@ function Page() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const [songs, setSongs] = useState([]);
+
+  const handleSongDeleted = (deletedSongId) => {
+    setSongs((prevSongs) =>
+      prevSongs.filter((song) => song.id !== deletedSongId)
+    );
+  };
 
   useEffect(() => {
     axios
@@ -141,7 +161,7 @@ function Page() {
       .catch((error) => {
         console.error("Error fetching songs:", error);
         setLoading(false);
-        setError("Error fetching songs. Please try again later.");
+        setError(error.message);
       });
   }, []);
 
@@ -151,7 +171,14 @@ function Page() {
         <div className='flex flex-col justify-center items-center gap-8 text-center'>
           {error && <ErrorMessage message={error} />}
           {!error &&
-            (loading ? <LoadingBar /> : <SongListDeleteForm songs={songs} />)}
+            (loading ? (
+              <LoadingBar />
+            ) : (
+              <SongListDeleteForm
+                songs={songs}
+                onSongDeleted={handleSongDeleted}
+              />
+            ))}
         </div>
       </div>
     </>
--- components/warning-dialog.jsx
+++ components/warning-dialog.jsx
@@ -3,22 +3,23 @@ import { Button } from "./ui/button";
 function WarningDialog({ title, message, onClickAction, onClickCancel }) {
   return (
     <div className='z-[999] fixed top-0 left-0 h-screen w-screen flex justify-center items-center bg-black/25 backdrop-blur animate-fade-in text-center'>
-      <div className='h-[25vh] w-[75vw] min-h-[150px] min-w-[250px] max-h-[200px] max-w-[350px] grid grid-cols-2 grid-rows-auto p-3 rounded-lg bg-dialogBlack border border-white items-center justify-center gap-x-3'>
+      <div className='h-[25vh] w-[75vw] min-h-[150px] min-w-[250px] max-h-[200px] max-w-[350px] grid grid-cols-2 grid-rows-auto p-5 rounded-lg bg-dialogBlack border border-white drop-shadow-md items-center justify-center gap-x-3'>
         <h1 className='text-white text-lg lg:text-xl row-start-1 col-span-2'>
           {title}
         </h1>
-        <p className=' text-white font-thin text-base lg:text-lg row-start-2 col-span-2'>
+        <p className=' text-white font-thin text-sm lg:text-base row-start-2 col-span-2'>
           {message}
         </p>
         <Button
-          className='bg-gray-500 text-black hover:bg-white duration-200 col-start-1 row-start-3'
+          className='bg-gray-600 text-black hover:bg-white duration-200 col-start-1 row-start-3 text-xs lg:text-base'
           onClick={onClickCancel}
         >
           Cancel
         </Button>
         <Button
-          className='bg-bpmPink text-black hover:bg-white duration-200 col-start-2 row-start-3'
+          className='bg-bpmPink text-black hover:bg-white duration-200 col-start-2 row-start-3 text-xs lg:text-base'
           onClick={onClickAction}
+          type='submit'
         >
           Continue
         </Button>
