fa3a435de25902624cc22d54c54474a89bc85c15
Shahar Yair <shaharyair12@gmail.com>
2023-08-19 16:18:51 +0300

    feat: checking if title and artist exists in ACRcloud
    user can add a track with the same title but a different artist

--- app/add-track/page.js
+++ app/add-track/page.js
@@ -14,20 +14,30 @@ import Tooltip from "@/components/tooltip";
 
 // Main component definition
 function Page() {
-  // State variables to manage form inputs, loading state, and error messages
+  // songs array, upload count to update songs with useEffect
   const [songs, setSongs] = useState([]);
+  const [uploadCount, setUploadCount] = useState(0);
+
+  // form values and options menu states
   const [selectedFile, setSelectedFile] = useState(null);
-  const [fileNameNoExtension, setFileNameNoExtension] = useState("");
+  const [fileName, setFileName] = useState("");
   const [songTitle, setSongTitle] = useState("");
   const [songArtist, setSongArtist] = useState("");
   const [songAlbum, setSongAlbum] = useState("");
+  const [optionsOpen, setOptionsOpen] = useState(false);
+
+  // open dialog messages states
   const [songUploaded, setSongUploaded] = useState("");
   const [error, setError] = useState("");
+
+  // loading state
   const [loading, setLoading] = useState(false);
-  const [optionsOpen, setOptionsOpen] = useState(false);
-  const [trackExists, setTrackExists] = useState(false);
+
+  // error handling states
+  const [trackExists, setTracksExists] = useState(false);
   const [userEditedTitle, setUserEditedTitle] = useState(false);
-  const [uploadCount, setUploadCount] = useState(0);
+  const [artistsByFilename, setArtistsByFilename] = useState("");
+  const [artistsBySongtitle, setartistsBySongtitle] = useState("");
 
   // Regular expression pattern and error message for text validation
   const regexPatternString = "^.{1,50}$";
@@ -42,18 +52,47 @@ function Page() {
   // Handler for file input change
   const handleFileChange = (event) => {
     // Update selected file and related state variables
+
     const newFile = event.target.files[0];
     setSelectedFile(newFile);
+
+    // get filename without the extension
+    const fileNameWithoutExtension =
+      newFile && newFile.name.replace(/\.[^/.]+$/, "");
+
+    // checking the artist names that has the same fileName
+    const selectedSongsByFilename =
+      fileNameWithoutExtension &&
+      songs.filter(
+        (song) =>
+          fileNameWithoutExtension.toLowerCase() === song.title.toLowerCase()
+      );
+
+    const matchingArtists =
+      selectedSongsByFilename &&
+      selectedSongsByFilename.map((song) => song.artist).join(", ");
+
+    setArtistsByFilename(matchingArtists);
+
+    // false if user didnt edit the title, title value will be the filename
     setUserEditedTitle(false);
-    const fileName = newFile && newFile.name.replace(/\.[^/.]+$/, "");
-    setFileNameNoExtension(fileName);
 
     // Check if the filename already exists in the database if title not edited by user
-    const noTitlefileNameExists =
-      fileName &&
+    const titleExists =
+      fileNameWithoutExtension &&
       !songTitle &&
-      songs.some((song) => song.title.toLowerCase() === fileName.toLowerCase());
-    setTrackExists(noTitlefileNameExists);
+      songs.some(
+        (song) =>
+          song.title.toLowerCase() === fileNameWithoutExtension.toLowerCase()
+      );
+    setTracksExists(titleExists);
+
+    // reset artist, title, album to an empty string / filename is set if there is a newfile
+    setSongArtist("");
+    setSongTitle("");
+    setSongAlbum("");
+    setFileName(newFile ? fileNameWithoutExtension : "");
+    setOptionsOpen(false);
   };
 
   // Handler for song title change
@@ -61,18 +100,86 @@ function Page() {
     // Update song title and check if it already exists
     const newTitle = event.target.value;
     setSongTitle(newTitle);
+
+    // checking the artist names that has the same songTitle
+    const selectedSongsBySongtitle =
+      newTitle &&
+      songs.filter(
+        (song) => newTitle.toLowerCase() === song.title.toLowerCase()
+      );
+
+    const matchingArtists =
+      selectedSongsBySongtitle &&
+      selectedSongsBySongtitle.map((song) => song.artist).join(", ");
+
+    setartistsBySongtitle(matchingArtists);
+
+    // true if user edited the title, title value will be the songTitle
     setUserEditedTitle(true);
 
     // Check if the title already exists in the database if title edited by user
-    const titleExists = songs.some(
-      (song) => song.title.toLowerCase() === newTitle.toLowerCase()
-    );
-    setTrackExists(titleExists);
+    const titleExists =
+      (newTitle &&
+        !songArtist &&
+        songs.some(
+          (song) => song.title.toLowerCase() === newTitle.toLowerCase()
+        )) ||
+      (fileName &&
+        !newTitle &&
+        !songArtist &&
+        songs.some(
+          (song) => song.title.toLowerCase() === fileName.toLowerCase()
+        )) ||
+      (newTitle &&
+        songArtist &&
+        songs.some(
+          (song) =>
+            song.title.toLowerCase() === newTitle.toLowerCase() &&
+            song.artist.toLowerCase() === songArtist.toLowerCase()
+        )) ||
+      (!newTitle &&
+        fileName &&
+        songArtist &&
+        songs.some(
+          (song) =>
+            song.title.toLowerCase() === fileName.toLowerCase() &&
+            song.artist.toLowerCase() === songArtist.toLowerCase()
+        ));
+    setTracksExists(titleExists);
   };
 
   // Handlers for artist and album input changes
   const handleArtistChange = (event) => {
-    setSongArtist(event.target.value);
+    const newArtist = event.target.value;
+    setSongArtist(newArtist);
+
+    const artistExists =
+      (songTitle &&
+        newArtist &&
+        songs.some(
+          (song) =>
+            song.title.toLowerCase() === songTitle.toLowerCase() &&
+            song.artist.toLowerCase() === newArtist.toLowerCase()
+        )) ||
+      (songTitle &&
+        !newArtist &&
+        songs.some(
+          (song) => song.title.toLowerCase() === songTitle.toLowerCase()
+        )) ||
+      (fileName &&
+        newArtist &&
+        !songTitle &&
+        songs.some(
+          (song) =>
+            song.title.toLowerCase() === fileName.toLowerCase() &&
+            song.artist.toLowerCase() === newArtist.toLowerCase()
+        )) ||
+      (fileName &&
+        !newArtist &&
+        songs.some(
+          (song) => song.title.toLowerCase() === fileName.toLowerCase()
+        ));
+    setTracksExists(artistExists);
   };
 
   const handleAlbumChange = (event) => {
@@ -93,8 +200,14 @@ function Page() {
       return;
     }
 
+    if (!songArtist) {
+      setError("Please add the track artist.");
+      setLoading(false);
+      return;
+    }
+
     if (
-      (fileNameNoExtension && !isValidText(fileNameNoExtension)) ||
+      (fileName && !isValidText(fileName)) ||
       (songTitle && !isValidText(songTitle)) ||
       (songArtist && !isValidText(songArtist)) ||
       (songAlbum && !isValidText(songAlbum))
@@ -107,30 +220,14 @@ function Page() {
     // Prepare form data for upload
     const formData = new FormData();
     formData.append("file", selectedFile);
-    formData.append("title", songTitle || fileNameNoExtension);
+    formData.append("title", songTitle || fileName);
     formData.append("artist", songArtist);
     formData.append("album", songAlbum);
 
     try {
-      // Check if the song with the same title or filename already exists
-      const songsResponse = await axios.get(
-        "http://127.0.0.1:5000/api/database_songs"
-      );
-      const songs = songsResponse.data;
-
-      const noTitlefileNameExists =
-        !songTitle &&
-        songs.some(
-          (song) =>
-            song.title.toLowerCase() === fileNameNoExtension.toLowerCase()
-        );
-
-      const titleExists = songs.some(
-        (song) => song.title.toLowerCase() === songTitle.toLowerCase()
-      );
-
-      if (titleExists || noTitlefileNameExists) {
-        setError("A track with the same title already exists.");
+      // Check if the song with the same title or artist already exists
+      if (trackExists) {
+        setError("A track with the same title and artist already exists.");
       } else {
         // Upload the song if the title doesn't exist
         await axios.post("http://127.0.0.1:5000/api/upload_song", formData);
@@ -144,7 +241,8 @@ function Page() {
       setUploadCount((prevCount) => prevCount + 1);
       setLoading(false);
       setOptionsOpen(false);
-      setFileNameNoExtension("");
+      setTracksExists(false);
+      setFileName("");
       setSongTitle("");
       setSongArtist("");
       setSongAlbum("");
@@ -164,6 +262,13 @@ function Page() {
         setError(error.message);
       });
   }, [uploadCount]);
+
+  // UseEffect to set OptionsOpen when trackExists changes
+  useEffect(() => {
+    if (trackExists) {
+      setOptionsOpen(true);
+    }
+  }, [trackExists]);
 
   // Render the component UI
   return (
@@ -198,12 +303,20 @@ function Page() {
                 onChange={handleFileChange}
                 required
               />
-              {/* Track exists message */}
+              {/* Title exists message */}
               {trackExists && (
-                <p className='text-red-800'>
-                  A track with the same title already exists.
-                </p>
+                <div className='text-red-800'>
+                  <p>
+                    This track title is already taken by&nbsp;
+                    <span className='text-white'>
+                      {`${artistsBySongtitle || artistsByFilename}`}
+                    </span>
+                    <br />
+                    Please choose a different title or artist.
+                  </p>
+                </div>
               )}
+
               {/* More Options section */}
               <div className='grid grid-col-2 justify-center items-center'>
                 {/* Toggle More Options */}
@@ -238,7 +351,7 @@ function Page() {
                     {/* Title input */}
                     <Input
                       type='text'
-                      value={userEditedTitle ? songTitle : fileNameNoExtension}
+                      value={userEditedTitle ? songTitle : fileName}
                       onChange={handleTitleChange}
                       placeholder='Title'
                       pattern={regexPatternString}
@@ -252,9 +365,13 @@ function Page() {
                       type='text'
                       value={songArtist}
                       onChange={handleArtistChange}
-                      placeholder='Artist'
+                      placeholder='Artist (Required)'
                       pattern={regexPatternString}
                       title={regexMessage}
+                      className={
+                        trackExists && "border-2 border-red-500 text-red-800"
+                      }
+                      required
                     />
                     {/* Album input */}
                     <Input
