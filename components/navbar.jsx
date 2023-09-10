96da5ffab03fc5e835bd7b15decd69369bf889c1
Shahar Yair <shaharyair12@gmail.com>
2023-09-10 20:36:23 +0300

    feat: improve ui/ux

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,7 +1,16 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { HiUser, HiBars3, HiXMark, HiMagnifyingGlass } from "react-icons/hi2";
+import { Squash as Hamburger } from "hamburger-react";
+import {
+  HiUser,
+  HiBars3,
+  HiXMark,
+  HiMagnifyingGlass,
+  HiMiniArrowUpTray,
+  HiTrash,
+  HiListBullet,
+} from "react-icons/hi2";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import SocialLinks from "./sociallinks";
@@ -56,75 +65,110 @@ function Navbar() {
        drop-shadow-lg backdrop-blur-sm z-50'
       >
         <div className='container flex items-center justify-between'>
-          <div
-            onClick={toggleMobileNavbar}
-            className='cursor-pointer lg:hidden text-3xl text-bpmPink'
-          >
-            {openMobileNavbar ? (
-              <HiXMark className='hover:text-white transition-colors duration-200' />
-            ) : (
-              <HiBars3 className='hover:text-white transition-colors duration-200' />
-            )}
+          <div className='text-bpmPink lg:hidden'>
+            <Hamburger
+              toggled={openMobileNavbar}
+              toggle={toggleMobileNavbar}
+              rounded
+              size={23}
+            />
           </div>
           <div
             className={`absolute ${
               openMobileNavbar
                 ? "left-1/4 md:left-1/2"
                 : "-left-3/4 md:-left-1/2"
-            } top-24 h-screen w-3/4 md:w-1/2 -translate-x-1/3 md:-translate-x-full transition-mobilenavbar lg:transition-none duration-500 bg-navbarBlack2  px-10 pt-10 lg:static lg:order-2 lg:mr-auto lg:h-auto lg:w-auto lg:translate-x-0 lg:bg-transparent lg:p-0`}
+            } flex flex-col items-start justify-start top-24 h-screen w-3/4 md:w-1/2 -translate-x-1/3 md:-translate-x-full transition-mobilenavbar lg:transition-none duration-500 bg-navbarBlack2  px-10 pt-10 lg:static lg:order-2 lg:mr-auto lg:h-auto lg:w-auto lg:translate-x-0 lg:bg-transparent lg:p-0`}
           >
-            <nav className='mb-40 lg:m-0'>
+            <nav>
               <ul className='flex flex-col items-start justify-center gap-3 text-left text-sm lg:text-base text-bpmPink lg:flex-row lg:items-center lg:gap-5 lg:text-center'>
                 <li>
                   <Link
-                    href='/tracks'
-                    className='block px-2 py-1 hover:text-white transition-colors duration-200'
+                    href='/'
+                    className='block p-0 lg:px-2 lg:py-1 hover:text-white transition-colors duration-200'
                   >
-                    Display Tracks
+                    Home
                   </Link>
                 </li>
                 <li>
                   <Link
-                    href='/add-track'
-                    className='block px-2 py-1 hover:text-white transition-colors duration-200'
+                    href='/'
+                    className='block p-0 lg:px-2 lg:py-1 hover:text-white transition-colors duration-200'
                   >
-                    Add Track
+                    Contact Us
                   </Link>
                 </li>
                 <li>
                   <Link
-                    href='/delete-track'
-                    className='block px-2 py-1 hover:text-white transition-colors duration-200'
+                    href='/'
+                    className='block p-0 lg:px-2 lg:py-1 hover:text-white transition-colors duration-200'
                   >
-                    Delete Track
+                    Services
                   </Link>
                 </li>
               </ul>
             </nav>
-            <Link href='/' className='inline-block lg:hidden'>
-              <div className='mb-10 flex items-center gap-3 text-bpmPink hover:text-white transition-colors duration-200 text-sm'>
-                <HiUser className='text-2xl' />
-                <p>Sign In</p>
-              </div>
-            </Link>
-            <div className='flex gap-4 lg:hidden text-2xl text-bpmPink'>
+            <div className='lg:hidden flex flex-col items-start justify-center gap-3 my-10 text-bpmPink text-lg'>
+              <Link
+                href='/tracks'
+                className='flex justify-center items-center gap-2 hover:text-white transition-colors duration-200 text-xl'
+              >
+                <HiListBullet />
+                <span className='text-sm'>Tracks</span>
+              </Link>
+              <Link
+                href='/add-track'
+                className='flex justify-center items-center gap-2 hover:text-white transition-colors duration-200 text-xl'
+              >
+                <HiMiniArrowUpTray />
+                <span className='text-sm'>Upload Track</span>
+              </Link>
+              <Link
+                href='/delete-track'
+                className='flex justify-center items-center gap-2 hover:text-white transition-colors duration-200 text-xl'
+              >
+                <HiTrash />
+                <span className='text-sm'>Delete Track</span>
+              </Link>
+            </div>
+            <div className='flex flex-row items-center gap-3 text-bpmPink hover:text-white transition-colors duration-200 text-sm lg:hidden'>
+              <Link
+                href='/'
+                className='flex justify-center items-center gap-2 hover:text-white transition-colors duration-200 text-xl'
+              >
+                <HiUser />
+                <span className='text-sm'>Sign In</span>
+              </Link>
+            </div>
+            <div className='flex gap-4 lg:hidden text-2xl text-bpmPink my-10'>
               <SocialLinks />
             </div>
           </div>
-          <Link href='/' className='ml-6 lg:ml-0 lg:mr-20'>
+          <Link href='/' className='lg:mr-20'>
             <h1 className='text-4xl lg:text-5xl font-semibold text-bpmPink'>
               Spot.
             </h1>
           </Link>
-          <div className='flex items-center justify-center gap-6 lg:order-3 text-2xl text-bpmPink lg:text-3xl'>
+          <div className='flex items-center justify-center gap-5 lg:order-3 text-bpmPink text-2xl'>
+            <div className='lg:flex items-center justify-center gap-5 text-bpmPink text-2xl hidden'>
+              <Link href='/tracks'>
+                <HiListBullet className=' hover:text-white transition-colors duration-200' />
+              </Link>
+              <Link href='/add-track'>
+                <HiMiniArrowUpTray className=' hover:text-white transition-colors duration-200' />
+              </Link>
+              <Link href='/delete-track'>
+                <HiTrash className=' hover:text-white transition-colors duration-200' />
+              </Link>
+              <Link href='/'>
+                <HiUser className='hover:text-white transition-colors duration-200' />
+              </Link>
+            </div>
             <Link href='/search'>
-              <Button className='bg-bpmPink rounded-xl text-black hover:bg-white duration-200 justify-center p-4 items-center gap-2 hidden lg:flex'>
+              <Button className='bg-bpmPink text-black hover:bg-white duration-200 rounded-xl justify-center hidden p-4 items-center gap-2 lg:flex lg:ml-2'>
                 Search <HiMagnifyingGlass className='text-lg' />
               </Button>
               <HiMagnifyingGlass className='lg:hidden hover:text-white transition-colors duration-200' />
-            </Link>
-            <Link href='/'>
-              <HiUser className='hover:text-white transition-colors duration-200' />
             </Link>
           </div>
         </div>
